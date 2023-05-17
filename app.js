087ffa6ccee521230cc4c8714b5f10db2662b0f3
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-05-16 19:52:46 -0500

    feat: create another route with express

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ import express from 'express'
 
 const app = express()
 
+//implementación de la ruta RAIZ (http://localhost:8080/)
 app.get('/', (request, response) => {
     response.send('Hola Mundo!!!')
+})
+
+//implementación de la otra ruta (http://localhost:8080/cursos)
+app.get('/cursos', (request, response) => {
+    response.send('Aquí van los cursos')
 })
 
 app.listen(8080, () => console.log('Server Up'))
