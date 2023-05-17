b60a2111748b89fab530c7270ac251d5bfded2c8
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-05-16 19:57:49 -0500

    feat: send different types of responses

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const app = express()
 
 //implementación de la ruta RAIZ (http://localhost:8080/)
 app.get('/', (request, response) => {
-    response.send('Hola Mundo!!!')
+    response.send('<h1>Hola Mundo!!!</h1><hr><div style="background-color: red">Soy un div</div>')
 })
 
 //implementación de la otra ruta (http://localhost:8080/cursos)
 app.get('/cursos', (request, response) => {
-    response.send('Aquí van los cursos')
+    response.send({cursos: ['Backend', 'ReactJs', 'AngularJs', 'NestJs']})
 })
 
 app.listen(8080, () => console.log('Server Up'))
